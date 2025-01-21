9639370cb1e80f94039eac61333502286dabe4ad
9Moses <esselmoses12@gmail.com>
2025-01-21 21:21:43 +0000

    update

--- app.ts
+++ app.ts
@@ -1,2 +1,34 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+//body praser
+app.use(express.json({ limit: "30mb" }));
+
+//cookie parser
+app.use(cookieParser());
+
+//cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//resting api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+//unkown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Rout ${req.originalUrl} not found`) as any;
+  err.statusCode = 400;
+  next(err);
+});
